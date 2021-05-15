bd872d8a376d32626cef367cbf13fc4b22d33a7f
Aquilla Silva Leite <aquilla11@hotmail.com>
2021-05-15 16:43:49 -0300

    adicionado test para o jenkins

---
+++ src/test/java/br/ce/wcaquino/taskbackend/model/TaskTest.java
@@ -0,0 +1,12 @@
+package br.ce.wcaquino.taskbackend.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TaskTest {
+	@Test
+	public void testTest() {
+		assertEquals(true, true);
+	}
+}
